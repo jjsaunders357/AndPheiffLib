d827daf5fa0fba2eb847a01b8792883cd4f64d73
Steve <Steve@Steve.com>
2016-03-30 01:03:10 -0400

    Changed LoggedActivity/Fragment to log to debug level.

--- stevelib/src/main/java/com/pheiffware/lib/and/fragments/pheiffListFragment/LoggedActivity.java
+++ stevelib/src/main/java/com/pheiffware/lib/and/fragments/pheiffListFragment/LoggedActivity.java
@@ -13,55 +13,55 @@ public class LoggedActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        Log.e("LifeC - " + getClass().getSimpleName(), "onCreate");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onCreate");
     }
 
     @Override
     protected void onStart()
     {
         super.onStart();
-        Log.e("LifeC - " + getClass().getSimpleName(), "onStart");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onStart");
     }
 
     @Override
     protected void onRestart()
     {
         super.onRestart();
-        Log.e("LifeC - " + getClass().getSimpleName(), "onRestart");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onRestart");
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
-        Log.e("LifeC - " + getClass().getSimpleName(), "onResume");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onResume");
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
-        Log.e("LifeC - " + getClass().getSimpleName(), "onPause");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onPause");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState)
     {
         super.onSaveInstanceState(outState);
-        Log.e("LifeC - " + getClass().getSimpleName(), "onSaveInstanceState");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onSaveInstanceState");
     }
 
     @Override
     protected void onStop()
     {
         super.onStop();
-        Log.e("LifeC - " + getClass().getSimpleName(), "onStop");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onStop");
     }
 
     @Override
     protected void onDestroy()
     {
         super.onDestroy();
-        Log.e("LifeC - " + getClass().getSimpleName(), "onDestroy");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onDestroy");
     }
 }
--- stevelib/src/main/java/com/pheiffware/lib/and/fragments/pheiffListFragment/LoggedFragment.java
+++ stevelib/src/main/java/com/pheiffware/lib/and/fragments/pheiffListFragment/LoggedFragment.java
@@ -16,14 +16,14 @@ public class LoggedFragment extends Fragment
     @Override
     public void onAttach(Context context)
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onAttach");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onAttach");
         super.onAttach(context);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onCreate");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onCreate");
         super.onCreate(savedInstanceState);
     }
 
@@ -31,7 +31,7 @@ public class LoggedFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onCreateView");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onCreateView");
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
@@ -39,56 +39,56 @@ public class LoggedFragment extends Fragment
     @Override
     public void onStart()
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onStart");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onStart");
         super.onStart();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState)
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onSaveInstanceState");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onSaveInstanceState");
         super.onSaveInstanceState(outState);
     }
 
     @Override
     public void onResume()
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onResume");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onResume");
         super.onResume();
     }
 
     @Override
     public void onPause()
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onPause");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onPause");
         super.onPause();
     }
 
     @Override
     public void onStop()
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onStop");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onStop");
         super.onStop();
     }
 
     @Override
     public void onDestroyView()
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onDestroyView");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onDestroyView");
         super.onDestroyView();
     }
 
     @Override
     public void onDestroy()
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onDestroy");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onDestroy");
         super.onDestroy();
     }
 
     @Override
     public void onDetach()
     {
-        Log.e("LifeC - " + getClass().getSimpleName(), "onDetach");
+        Log.d("LifeC - " + getClass().getSimpleName(), "onDetach");
         super.onDetach();
     }
 }
