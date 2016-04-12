b56150c8a95b66f0c7a2f9edfe0061c271b37189
Steve <Steve@Steve.com>
2016-04-11 22:38:39 -0400

    Updated Collada parsing of texture image file name.  It now ignores any leading directory.  The assumption is that all textures will be loaded from a single known path.

--- stevelib/src/main/java/com/pheiffware/lib/geometry/collada/ColladaLibraryImageFactory.java
+++ stevelib/src/main/java/com/pheiffware/lib/geometry/collada/ColladaLibraryImageFactory.java
@@ -7,7 +7,8 @@ import com.pheiffware.lib.utils.dom.XMLParseException;
 import org.w3c.dom.Element;
 
 /**
- * Used to extract image file name from an image element.
+ * Used to extract image file name from an image element.  This always removes any leading path, as the assumption is that all files will be loaded from a single directory.
+ * <p/>
  * Created by Steve on 2/15/2016.
  */
 class ColladaLibraryImageFactory implements ElementObjectFactory<String>
@@ -17,11 +18,13 @@ class ColladaLibraryImageFactory implements ElementObjectFactory<String>
     {
         Element init_from = DomUtils.assertGetSubElement(element, "init_from");
         String imageFileName = init_from.getFirstChild().getTextContent();
+        String[] split = imageFileName.split("/");
+        imageFileName = split[split.length - 1];
         return imageFileName;
     }
 }
 /*Example:
     <image id="ID13">
-        <init_from>test_sk/Steel.png</init_from>
+        <init_from>ignore/this/path/Steel.png</init_from>
     </image>
  */
